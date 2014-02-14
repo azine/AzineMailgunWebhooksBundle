065fd9b580a53b01d38be2a7a29db5f0d59a22cc
dominik <dominik.businger@azine-it.ch>
2014-02-14 12:06:07 +0100

    improve docs & tests

--- Services/AzineMailgunService.php
+++ Services/AzineMailgunService.php
@@ -28,10 +28,25 @@ class AzineMailgunService {
 	}
 
 	/**
-	 * Deletes all MailgunEvents of the given type that are older than the ageLimit
-	 * @param string $type
+	 * Deletes all MailgunEvents of the given type(s) that are older than the ageLimit
+	 * 
+	 * Possible Types are:
+	 * 
+	 * Event-Type		Description
+	 * ---------------------------------------------------------------------
+	 * accepted 		Mailgun accepted the request to send/forward the email and the message has been placed in queue.
+	 * rejected 		Mailgun rejected the request to send/forward the email.
+	 * delivered 		Mailgun sent the email and it was accepted by the recipient email server.
+	 * failed 			Mailgun could not deliver the email to the recipient email server.
+	 * opened 			The email recipient opened the email and enabled image viewing. Open tracking must be enabled in the Mailgun control panel, and the CNAME record must be pointing to mailgun.org.
+	 * clicked 		The email recipient clicked on a link in the email. Click tracking must be enabled in the Mailgun control panel, and the CNAME record must be pointing to mailgun.org.
+	 * unsubscribed 	The email recipient clicked on the unsubscribe link. Unsubscribe tracking must be enabled in the Mailgun control panel.
+	 * complained 		The email recipient clicked on the spam complaint button within their email client. Feedback loops enable the notification to be received by Mailgun.
+	 * stored 			Mailgun has stored an incoming message
+	 * 
+	 * @param string|array of string|null $type  
 	 * @param \DateTime $ageLimit
-	 * @return
+	 * @return integer number of deleted records
 	 */
 	public function removeEvents($type = null, \DateTime $ageLimit){
 
@@ -40,8 +55,11 @@ class AzineMailgunService {
 			->andWhere("e.timestamp < :age")
 			->setParameter("age", $ageLimit->getTimestamp());
 
-		if($type != null){
+		if(is_string($type)){
 			$qb->andWhere("e.event = :type")
+				->setParameter("type", $type);
+		} else if (is_array($type)){
+			$qb->andWhere("e.event in (:type)")
 				->setParameter("type", $type);
 		}
 
--- Tests/Services/AzineMailgunServiceTest.php
+++ Tests/Services/AzineMailgunServiceTest.php
@@ -12,11 +12,11 @@ class AzineMailgunServiceTest extends \PHPUnit_Framework_TestCase {
 	public function testRemoveOldEventEntries(){
 
 		$ageLimit = new \DateTime("5 days ago");
-
+		$count = 23;
 
 		$q = $this->getMockBuilder("Azine\MailgunWebhooksBundle\Tests\Services\AzineQueryMock")->disableOriginalConstructor()->getMock();
-		$q->expects($this->once())->method("execute");
-
+		$q->expects($this->once())->method("execute")->will($this->returnValue($count));
+		
 		$qb = $this->getMockBuilder("Doctrine\ORM\QueryBuilder")->disableOriginalConstructor()->getMock();
 		$qb->expects($this->once())->method("delete")->with("Azine\MailgunWebhooksBundle\Entity\MailgunEvent", "e")->will($this->returnValue($qb));
 		$qb->expects($this->once())->method("andWhere")->with("e.timestamp < :age")->will($this->returnValue($qb));
@@ -30,17 +30,19 @@ class AzineMailgunServiceTest extends \PHPUnit_Framework_TestCase {
 		$registry->expects($this->once())->method("getManager")->will($this->returnValue($em));
 
 		$amgs = new AzineMailgunService($registry);
-		$amgs->removeOldEventEntries($ageLimit);
-
+		$deleteCount = $amgs->removeOldEventEntries($ageLimit);
+		$this->assertEquals($count, $deleteCount, "Expected that $count entries are reported as deleted.");
+		
 	}
 
-	public function testRemoveOldEventEntriesByType(){
+	public function testRemoveOldEventEntriesByType_with_string(){
 
 		$ageLimit = new \DateTime("5 days ago");
 		$type = "bounced";
+		$count = 12;
 
 		$q = $this->getMockBuilder("Azine\MailgunWebhooksBundle\Tests\Services\AzineQueryMock")->disableOriginalConstructor()->getMock();
-		$q->expects($this->once())->method("execute");
+		$q->expects($this->once())->method("execute")->will($this->returnValue($count));
 
 		$qb = $this->getMockBuilder("Doctrine\ORM\QueryBuilder")->disableOriginalConstructor()->getMock();
 		$qb->expects($this->once())->method("delete")->with("Azine\MailgunWebhooksBundle\Entity\MailgunEvent", "e")->will($this->returnValue($qb));
@@ -55,7 +57,34 @@ class AzineMailgunServiceTest extends \PHPUnit_Framework_TestCase {
 		$registry->expects($this->once())->method("getManager")->will($this->returnValue($em));
 
 		$amgs = new AzineMailgunService($registry);
-		$amgs->removeEvents($type, $ageLimit);
+		$deleteCount = $amgs->removeEvents($type, $ageLimit);
+		$this->assertEquals($count, $deleteCount, "Expected that $count entries are reported as deleted.");
 
 	}
-}
+
+	public function testRemoveOldEventEntriesByType_with_array(){
+
+		$ageLimit = new \DateTime("5 days ago");
+		$type = array("bounced","dropped");
+		$count = 12;
+		
+		$q = $this->getMockBuilder("Azine\MailgunWebhooksBundle\Tests\Services\AzineQueryMock")->disableOriginalConstructor()->getMock();
+		$q->expects($this->once())->method("execute")->will($this->returnValue($count));
+		
+		$qb = $this->getMockBuilder("Doctrine\ORM\QueryBuilder")->disableOriginalConstructor()->getMock();
+		$qb->expects($this->once())->method("delete")->with("Azine\MailgunWebhooksBundle\Entity\MailgunEvent", "e")->will($this->returnValue($qb));
+		$qb->expects($this->exactly(2))->method("andWhere")->with()->will($this->returnValueMap(array(array("e.timestamp < :age", $qb), array("e.event in (:type)", $qb))));
+		$qb->expects($this->exactly(2))->method("setParameter")->will($this->returnValueMap(array(array("age", $ageLimit->getTimestamp(), null, $qb), array("type", $type, null, $qb))));
+		$qb->expects($this->once())->method("getQuery")->will($this->returnValue($q));
+
+		$em = $this->getMockBuilder("Doctrine\ORM\EntityManager")->disableOriginalConstructor()->getMock();
+		$em->expects($this->once())->method("createQueryBuilder")->will($this->returnValue($qb));
+
+		$registry = $this->getMockBuilder("Doctrine\Bundle\DoctrineBundle\Registry")->disableOriginalConstructor()->getMock();
+		$registry->expects($this->once())->method("getManager")->will($this->returnValue($em));
+
+		$amgs = new AzineMailgunService($registry);
+		$deleteCount = $amgs->removeEvents($type, $ageLimit);
+		$this->assertEquals($count, $deleteCount, "Expected that $count entries are reported as deleted.");
+		
+	}}
