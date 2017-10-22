5314dc6b71ba692279de4eb3c7d04b5ac1cd18c0
eugene-o-z <webchoise@gmail.com>
2017-10-22 17:42:45 +0300

    prevent flood spam report

--- DependencyInjection/AzineMailgunWebhooksExtension.php
+++ DependencyInjection/AzineMailgunWebhooksExtension.php
@@ -16,7 +16,8 @@ class AzineMailgunWebhooksExtension extends Extension
     const PREFIX = "azine_mailgun_webhooks";
     const API_KEY = "api_key";
     const PUBLIC_API_KEY = "public_api_key";
-    const SEND_NOTIFICATIONS = "send_notifications";
+    const SEND_SPAM_ALERTS = "send_spam_alerts";
+    const SEND_SPAM_ALERTS_INTERVAL = "send_spam_alerts_interval";
     const TICKET_ID = "ticket_id";
     const TICKET_SUBJECT = "ticket_subject";
     const TICKET_MESSAGE = "ticket_message";
@@ -37,7 +38,8 @@ class AzineMailgunWebhooksExtension extends Extension
             $container->setParameter(self::PREFIX."_".self::PUBLIC_API_KEY, $config[self::PUBLIC_API_KEY]);
         
         $container->setParameter(self::PREFIX."_".self::TICKET_ID, $config[self::TICKET_ID]);
-        $container->setParameter(self::PREFIX."_".self::SEND_NOTIFICATIONS, $config[self::SEND_NOTIFICATIONS]);
+        $container->setParameter(self::PREFIX."_".self::SEND_SPAM_ALERTS, $config[self::SEND_SPAM_ALERTS]);
+        $container->setParameter(self::PREFIX."_".self::SEND_SPAM_ALERTS_INTERVAL, $config[self::SEND_SPAM_ALERTS_INTERVAL]);
         $container->setParameter(self::PREFIX."_".self::TICKET_SUBJECT, $config[self::TICKET_SUBJECT]);        
         $container->setParameter(self::PREFIX."_".self::TICKET_MESSAGE, $config[self::TICKET_MESSAGE]);        
         $container->setParameter(self::PREFIX."_".self::ADMIN_USER_EMAIL, $config[self::ADMIN_USER_EMAIL]);
--- Services/AzineMailgunMailerService.php
+++ Services/AzineMailgunMailerService.php
@@ -7,6 +7,7 @@ use Monolog\Logger;
 use Symfony\Component\Translation\Translator;
 use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Azine\MailgunWebhooksBundle\Entity\EmailTrafficStatistics;
 
 class AzineMailgunMailerService
 {
@@ -20,7 +21,9 @@ class AzineMailgunMailerService
     private $ticketSubject;
     private $ticketMessage;
     private $adminUserEmail;
-    
+    private $entityManager;
+    private $sendNotificationsInterval;
+
     public function __construct(
         \Swift_Mailer $mailer,
         ValidatorInterface $validator,
@@ -31,7 +34,9 @@ class AzineMailgunMailerService
         $ticketId,
         $ticketSubject,
         $ticketMessage,
-        $adminUserEmail
+        $adminUserEmail,
+        $entityManager,
+        $sendNotificationsInterval
     ) {
         $this->mailer = $mailer;
         $this->validator = $validator;
@@ -43,32 +48,65 @@ class AzineMailgunMailerService
         $this->ticketSubject = $ticketSubject;
         $this->ticketMessage = $ticketMessage;
         $this->adminUserEmail = $adminUserEmail;
+        $this->entityManager = $entityManager;
+        $this->sendNotificationsInterval = $sendNotificationsInterval;
     }
 
     public function  sendSpamComplaintNotification($eventId)
     {
+        $messagesSent = 0;
         $emailValidityErrors = $this->validateEmail($this->adminUserEmail);
-        if (is_null($emailValidityErrors)) {            
+
+        if (is_null($emailValidityErrors)) {
             /** @var \Swift_Message $message */
             $message = $this->mailer->createMessage();
             $message->setTo($this->adminUserEmail)
-                    ->setFrom($this->mailerUser)
-                    ->setSubject($this->translator->trans('notification.spam_complaint_received'))
-                    ->setBody(
-                        $this->twig->render('@AzineMailgunWebhooks/Email/notification.html.twig', array('eventId' => $eventId, 'ticketId' => $this->ticketId)),
-                        'text/html'
-                    );
-            
-            $this->mailer->send($message);
+                ->setFrom($this->mailerUser)
+                ->setSubject($this->translator->trans('notification.spam_complaint_received'))
+                ->setBody(
+                    $this->twig->render('@AzineMailgunWebhooks/Email/notification.html.twig', array('eventId' => $eventId, 'ticketId' => $this->ticketId)),
+                    'text/html'
+                );
+
+            $lastSpamReport = $this->entityManager->getRepository(EmailTrafficStatistics::class)
+                ->findOneBy(['action' => EmailTrafficStatistics::SPAM_ALERT_SENT],
+                    ['created' => 'DESC']);
+
+
+            if($lastSpamReport instanceof EmailTrafficStatistics) {
+
+                $time = new \DateTime();
+                $timeDiff = $time->diff($lastSpamReport->getCreated());
+
+                if($timeDiff->i > $this->sendNotificationsInterval) {
+
+                    $messagesSent = $this->mailer->send($message);
+                }
+
+            }else{
+
+                $messagesSent = $this->mailer->send($message);
+            }
+
+            if($messagesSent > 0) {
+
+                $spamAlert = new EmailTrafficStatistics();
+                $spamAlert->setAction(EmailTrafficStatistics::SPAM_ALERT_SENT);
+                $this->entityManager->persist($spamAlert);
+                $this->entityManager->flush($spamAlert);
+                $this->entityManager->clear();
+            }
         } else {
             $this->logger->warning('Tried to send notification about spam complaint but adminUserEmail is invalid: ' . json_encode($emailValidityErrors));
         }
+
+        return $messagesSent;
     }
 
     private function validateEmail($email)
     {
         $errors = $this->validator->validate(
-            $email, 
+            $email,
             array(
                 new \Symfony\Component\Validator\Constraints\Email(),
                 new \Symfony\Component\Validator\Constraints\NotBlank()
