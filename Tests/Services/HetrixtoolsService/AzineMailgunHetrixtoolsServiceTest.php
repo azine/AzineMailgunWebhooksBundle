ee5d32af378508c4fd7bd7c06e52fbdc32cc949c
eugene-o-z <webchoise@gmail.com>
2017-11-28 19:30:11 +0200

    testing of successful response

--- Tests/Services/HetrixtoolsService/AzineMailgunHetrixtoolsServiceTest.php
+++ Tests/Services/HetrixtoolsService/AzineMailgunHetrixtoolsServiceTest.php
@@ -7,6 +7,33 @@ use Azine\MailgunWebhooksBundle\Services\HetrixtoolsService\HetrixtoolsServiceRe
 
 class AzineMailgunHetrixtoolsServiceTest extends \PHPUnit_Framework_TestCase
 {
+    private $responseJson = '{
+            "status": "SUCCESS",
+            "api_calls_left": 1976,
+            "blacklist_check_credits_left": 81,
+            "blacklisted_count": 3,
+            "blacklisted_on": [
+                {
+                    "rbl": "example1.com",
+                    "delist": "https://example1.com/ip/44.44.44.44"
+                },
+                {
+                    "rbl": "example2.org",
+                    "delist": "https://www.example2.org/query/ip/44.44.44.44"
+                },
+                {
+                    "rbl": "example3.org",
+                    "delist": "https://www.example3.org/query/ip/44.44.44.44"
+                }
+            ],
+            "links": {
+                "report_link": "https://example3.com/report/blacklist/token/",
+                "whitelabel_report_link": "",
+                "api_report_link": "https://api.example3.com/v1/token/report/44.44.44.44/",
+                "api_blacklist_check_link": "https://api.example3.com/v2/token/blacklist-check/ipv4/44.44.44.44/"
+            }
+        }';
+
     /**
      * @expectedException \InvalidArgumentException
      */
@@ -37,5 +64,21 @@ class AzineMailgunHetrixtoolsServiceTest extends \PHPUnit_Framework_TestCase
 
         $ip = 'invalidIpAddress';
         $service->checkIpAddressInBlacklist($ip);
+    }
+
+    public function testSuccessReponse()
+    {
+        $apiKey = 'test';
+        $url = 'https://api.example.com/v2/testkey/blacklist-check/ipv4/44.44.44.44/';
+
+        $hetrixtoolsService = $this->getMockBuilder("Azine\MailgunWebhooksBundle\Services\HetrixtoolsService\AzineMailgunHetrixtoolsService")
+            ->setConstructorArgs([$apiKey, $url])
+            ->setMethods(array('get'))->getMock();
+        $hetrixtoolsService->expects($this->once())->method("get")->will($this->returnValue($this->responseJson));
+
+
+        $response = $hetrixtoolsService->checkIpAddressInBlacklist('44.44.44.44');
+
+        $this->assertInstanceOf(HetrixtoolsServiceResponse::class, $response);
     }
 }
