b97ecc99a760a39b9024418e2384f0badf6e0e3f
eugene-o-z <webchoise@gmail.com>
2017-11-28 12:20:13 +0200

    replacing of fake domain names

--- Tests/Services/HetrixtoolsService/HetrixtoolsServiceResponseTest.php
+++ Tests/Services/HetrixtoolsService/HetrixtoolsServiceResponseTest.php
@@ -15,23 +15,23 @@ class HetrixtoolsServiceResponseTest extends \PHPUnit_Framework_TestCase
             "blacklisted_count": 3,
             "blacklisted_on": [
                 {
-                    "rbl": "dnsbl.test.com",
-                    "delist": "https://exchange.test.com/ip/44.44.44.44"
+                    "rbl": "example1.com",
+                    "delist": "https://example1.com/ip/44.44.44.44"
                 },
                 {
-                    "rbl": "pbl.test.org",
-                    "delist": "https://www.test.org/query/ip/44.44.44.44"
+                    "rbl": "example2.org",
+                    "delist": "https://www.example2.org/query/ip/44.44.44.44"
                 },
                 {
-                    "rbl": "zen.test.org",
-                    "delist": "https://www.test.org/query/ip/44.44.44.44"
+                    "rbl": "example3.org",
+                    "delist": "https://www.example3.org/query/ip/44.44.44.44"
                 }
             ],
             "links": {
-                "report_link": "https://test.com/report/blacklist/token/",
+                "report_link": "https://example3.com/report/blacklist/token/",
                 "whitelabel_report_link": "",
-                "api_report_link": "https://api.test.com/v1/token/report/44.44.44.44/",
-                "api_blacklist_check_link": "https://api.test.com/v2/token/blacklist-check/ipv4/44.44.44.44/"
+                "api_report_link": "https://api.example3.com/v1/token/report/44.44.44.44/",
+                "api_blacklist_check_link": "https://api.example3.com/v2/token/blacklist-check/ipv4/44.44.44.44/"
             }
         }';
 
