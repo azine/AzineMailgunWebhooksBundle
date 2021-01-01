1a2a1b54b49917b22202ebfb8fc9083fd5c60406
azine.me <github@azine-it.ch>
2021-01-01 23:04:38 +0100

    update composer.json to include components instead of symfony/symfony (#53)

--- Tests/Controller/MailgunEventControllerTest.php
+++ Tests/Controller/MailgunEventControllerTest.php
@@ -365,7 +365,7 @@ class MailgunWebhookControllerTest extends WebTestCase
     {
         try {
             static::$kernel = static::createKernel(array());
-            //static::$kernel->boot();
+            static::$kernel->boot();
         } catch (\RuntimeException $ex) {
             $this->markTestSkipped('There does not seem to be a full application available (e.g. running tests on travis.org). So this test is skipped.');
 
